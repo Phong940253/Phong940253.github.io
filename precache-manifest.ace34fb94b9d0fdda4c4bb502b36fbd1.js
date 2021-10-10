00234314220d29b4d46a29fc8a807781aaa9721b
Phong <phong940253@gmail.com>
2021-10-10 20:28:33 +0700

    init

---
+++ precache-manifest.ace34fb94b9d0fdda4c4bb502b36fbd1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b28470576f56d46ca69a0fffe8f4c676",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a77344f107c37eb1a35",
+    "url": "/static/css/main.8a45dbbb.chunk.css"
+  },
+  {
+    "revision": "1cca05fda98134adf657",
+    "url": "/static/js/2.7b0d2407.chunk.js"
+  },
+  {
+    "revision": "82e108a1263930ca779ba3f21ed959ea",
+    "url": "/static/js/2.7b0d2407.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a77344f107c37eb1a35",
+    "url": "/static/js/main.3d4b88fb.chunk.js"
+  },
+  {
+    "revision": "ed8ed874e871633992a1",
+    "url": "/static/js/runtime-main.a418149b.js"
+  },
+  {
+    "revision": "422fec1079cce450353e924288f8313b",
+    "url": "/static/media/commerce.422fec10.png"
+  }
+]);
